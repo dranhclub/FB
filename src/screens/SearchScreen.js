db93de6447f49867d4966ea8f689e1190b7bf230
dranhclub <58360612+dranhclub@users.noreply.github.com>
2020-11-10 15:01:14 +0700

    edit search screen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,90 @@
+/*
+  Màn hình tìm kiếm
+*/ 
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { View, Text, StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Post from '../components/Post';
 
 const Tab = createMaterialTopTabNavigator();
+const examplePeople = [
+  {
+    name: 'Minh Ánh',
+    avatar: 'https://picsum.photos/seed/minhanh/200/200',
+    description: 'Bạn bè - Vinh'
+  },
+  {
+    name: 'Ngọc Ánh',
+    avatar: 'https://picsum.photos/seed/ngocanh/200/200',
+    description: 'Bạn bè - Hà Nội'
+  },
+];
+
+function PeopleView() {
+  return(
+    <View style={{ backgroundColor: 'white', borderRadius: 10, margin: 10 }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 20, padding: 20 }}>Mọi người</Text>
+      {
+        examplePeople.map((item, index) => {
+          return (
+            <TouchableOpacity key={index}>
+              <View style={{ flexDirection: 'row', alignItems: 'center', padding: 10 }}>
+                <Image
+                  style={{ width: 60, height: 60, borderRadius: 100 }}
+                  source={{ uri: item.avatar }}
+                />
+                <View style={{ flex: 1, paddingHorizontal: 10 }}>
+                  <Text style={{ fontWeight: 'bold', fontSize: 16 }}>{item.name}</Text>
+                  <Text>{item.description}</Text>
+                </View>
+                <FontAwesome5 name='facebook-messenger' size={20} />
+              </View>
+            </TouchableOpacity>
+          )
+        })
+      }
+    </View>
+  );
+}
+
+function PostView() {
+  return(
+    <View style={{ backgroundColor: 'white', borderRadius: 10, margin: 10 }}>
+      <Text style={{ fontWeight: 'bold', fontSize: 20, padding: 20 }}>Bài viết</Text>
+      <Post
+        displayName='Minh Ánh'
+        text='Non minim pariatur voluptate est. Magna eiusmod dolore Lorem nulla ad ex quis officia incididunt irure do pariatur fugiat. Ex ut commodo veniam ad commodo. Consequat irure culpa dolore commodo.'
+        key='postId2'
+        time='Vừa xong'
+      />
+    </View>
+  );
+}
 
 function ShowAll() {
   return (
-    <View>
-      <Text>Show all</Text>
-    </View>
+    <ScrollView>
+      <PeopleView />
+      <PostView />
+    </ScrollView>
   );
 }
 
 function Posts() {
   return (
-    <View><Text>Bài viết</Text></View>
+    <ScrollView>
+      <PostView />
+    </ScrollView>
   );
 }
 
 function People() {
   return (
-    <View><Text>Mọi người</Text></View>
+    <ScrollView>
+      <PeopleView />
+    </ScrollView>
   );
 }
 
@@ -30,7 +92,7 @@ export function SearchBar() {
   return (
     <View style={styles.searchBar}>
       <FontAwesome5 name='search' color='#757575' size={20} />
-      <TextInput placeholder='Tìm kiếm' />
+      <TextInput placeholder='Tìm kiếm' value='ánh'/>
     </View>
   );
 };
