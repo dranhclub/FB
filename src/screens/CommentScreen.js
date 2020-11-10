767d657e5ba79052a34223eef82db16e4b3f036c
dranhclub <58360612+dranhclub@users.noreply.github.com>
2020-11-10 20:47:36 +0700

    add comment input text

--- src/screens/CommentScreen.js
+++ src/screens/CommentScreen.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import { ScrollView, TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import { Text, StyleSheet, Image, View } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 function Comment({avatar, name, text, time}) {
   const styles = StyleSheet.create({
     container: {
       flexDirection: 'row',
       marginBottom: 15,
-      paddingRight: 60
+      paddingRight: 60,
     },
     avatar: {
       width: 50,
       height: 50,
       borderRadius: 25
+    },
+    content: {
+      backgroundColor: '#eee', 
+      borderRadius: 20, 
+      paddingHorizontal: 10, 
+      paddingVertical: 8
     }
   });
   
@@ -20,13 +27,13 @@ function Comment({avatar, name, text, time}) {
     <View style={styles.container}>
       <Image source={avatar} style={styles.avatar}/>
       <View style={{marginLeft: 10}}>
-        <View style={{backgroundColor: '#eee', borderRadius: 20, padding: 10}}>
-          <Text style={{fontWeight: 'bold', fontSize: 16}}>{name}</Text>
+        <View style={styles.content}>
+          <Text style={{fontWeight: 'bold', fontSize: 14}}>{name}</Text>
           <Text style={{flexWrap: 'wrap'}}>{text}</Text>
         </View>
         <View style={{flexDirection: 'row'}}>
-          <Text>{time}</Text>
-          <TouchableOpacity onPress={()=>alert('Đã thích :D')}>
+          <Text style={{color: '#757575'}}>{time}</Text>
+          <TouchableOpacity>
             <Text style={{fontWeight: 'bold', marginLeft: 20}}>Thích</Text>
           </TouchableOpacity>
         </View>
@@ -35,31 +42,84 @@ function Comment({avatar, name, text, time}) {
   );
 }
 
-const exampleAvatars = [
-  {uri: 'https://picsum.photos/seed/picsum1/50/50'},
-  {uri: 'https://picsum.photos/seed/picsum2/50/50'},
-  {uri: 'https://picsum.photos/seed/picsum3/50/50'},
+const exampleComments = [
+  {
+    name:'Nguyễn Tuấn Anh',
+    avatar: {uri: 'https://picsum.photos/seed/picsum1/50/50'},
+    text: '😂😂😂',
+    time: '2 phút trước'
+  },
+  {
+    name:'Nguyễn Ngọc Đức Thắng',
+    avatar: {uri: 'https://picsum.photos/seed/picsum2/50/50'},
+    text: 'Voluptate eiusmod sunt magna quis ipsum occaecat ea.',
+    time: '2 phút trước'
+  },
+  {
+    name:'Huyền Nguyễn',
+    avatar: {uri: 'https://picsum.photos/seed/picsum3/50/50'},
+    text: 'Ad sunt laboris consectetur reprehenderit aliquip ut consectetur ut cupidatat in proident aute dolor.',
+    time: '4 phút trước'
+  },
+  {
+    name:'Trang Nguyen',
+    avatar: {uri: 'https://picsum.photos/seed/picsum4/50/50'},
+    text: 'Commodo esse ullamco laborum aliquip amet.',
+    time: '5 phút trước'
+  },
+  {
+    name:'Nguyễn Tuấn Anh',
+    avatar: {uri: 'https://picsum.photos/seed/picsum1/50/50'},
+    text: '😂😂😂',
+    time: '2 phút trước'
+  },
+  {
+    name:'Nguyễn Ngọc Đức Thắng',
+    avatar: {uri: 'https://picsum.photos/seed/picsum2/50/50'},
+    text: 'Voluptate eiusmod sunt magna quis ipsum occaecat ea.',
+    time: '2 phút trước'
+  },
+  {
+    name:'Huyền Nguyễn',
+    avatar: {uri: 'https://picsum.photos/seed/picsum3/50/50'},
+    text: 'Ad sunt laboris consectetur reprehenderit aliquip ut consectetur ut cupidatat in proident aute dolor.',
+    time: '4 phút trước'
+  },
+  {
+    name:'Trang Nguyen',
+    avatar: {uri: 'https://picsum.photos/seed/picsum4/50/50'},
+    text: 'Commodo esse ullamco laborum aliquip amet.',
+    time: '5 phút trước'
+  },
 ]
 
 export default function CommentScreen() {
   return (
-    <ScrollView style={styles.container}>
-      <Comment name={'Nguyễn Ngọc Đức Thắng'} avatar={exampleAvatars[0]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Ngô Huy Thao'} avatar={exampleAvatars[1]} text={'Sit commodo cupidatat elit aute dolor veniam esse deserunt esserunt esserunt esserunt esse eiusmod quis ad enim.'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Văn Tráng'} avatar={exampleAvatars[2]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Ngọc Đức Thắng'} avatar={exampleAvatars[0]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Ngô Huy Thao'} avatar={exampleAvatars[1]} text={'Sit commodo cupidatat elit aute dolor veniam esse deserunt esserunt esserunt esserunt esse eiusmod quis ad enim.'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Văn Tráng'} avatar={exampleAvatars[2]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Ngọc Đức Thắng'} avatar={exampleAvatars[0]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Ngô Huy Thao'} avatar={exampleAvatars[1]} text={'Sit commodo cupidatat elit aute dolor veniam esse deserunt esserunt esserunt esserunt esse eiusmod quis ad enim.'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Văn Tráng'} avatar={exampleAvatars[2]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Ngọc Đức Thắng'} avatar={exampleAvatars[0]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Ngô Huy Thao'} avatar={exampleAvatars[1]} text={'Sit commodo cupidatat elit aute dolor veniam esse deserunt esserunt esserunt esserunt esse eiusmod quis ad enim.'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Văn Tráng'} avatar={exampleAvatars[2]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Ngọc Đức Thắng'} avatar={exampleAvatars[0]} text={'Chào cậu'} time={'2 phút trước'}/>
-      <Comment name={'Ngô Huy Thao'} avatar={exampleAvatars[1]} text={'Sit commodo cupidatat elit aute dolor veniam esse deserunt esserunt esserunt esserunt esse eiusmod quis ad enim.'} time={'2 phút trước'}/>
-      <Comment name={'Nguyễn Văn Tráng'} avatar={exampleAvatars[2]} text={'Chào cậu'} time={'2 phút trước'}/>
-    </ScrollView>
+    <View style={{flex: 1}}>
+      {/* Comments list */}
+      <ScrollView style={styles.container}>
+        {exampleComments.map((item, index)=>{
+          return(
+            <Comment 
+              key={index}
+              name={item.name} 
+              avatar={item.avatar} 
+              text={item.text} 
+              time={item.time}/>
+          );
+        })}
+      </ScrollView>
+
+      {/* Comment input text */}
+      <View style={styles.commentInputTextWrapper}>
+        <View style={styles.commentInputText}>
+          <TextInput placeholder='Bình luận' multiline style={{flex: 1, paddingHorizontal: 10}}/>
+          <TouchableOpacity>
+            <FontAwesome5 name='paper-plane' size={20} color={'#2979FF'} solid style={{paddingHorizontal: 20}}/>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
   );
 }
 
@@ -68,4 +128,16 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: 'white'
   },
+  commentInputTextWrapper: {
+    backgroundColor: 'white', 
+    borderTopColor: '#ccc', 
+    borderTopWidth: 1, 
+    padding: 5
+  },
+  commentInputText: {
+    backgroundColor: '#eee',
+    borderRadius: 25,
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 });
