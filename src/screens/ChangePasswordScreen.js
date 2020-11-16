541bf37488945daa6f1d4c87ecf4904456708f89
ducanh <ducanhdt>
2020-11-16 14:02:13 +0700

    add backend changePassword

--- src/screens/ChangePasswordScreen.js
+++ src/screens/ChangePasswordScreen.js
@@ -1,80 +1,153 @@
-import React, { useEffect, useState } from 'react'
-import { Button, StyleSheet, Text, TextInput, View, Dimensions, KeyboardAvoidingView, Keyboard} from 'react-native'
+import React, {useEffect, useState} from 'react';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Dimensions,
+  KeyboardAvoidingView,
+  Keyboard,
+} from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
-import { AuthContext, RegisterContext } from '../contexts/MyContexts';
-import { WarningComponent } from './LoginScreen';
+import {AuthContext, RegisterContext} from '../contexts/MyContexts';
+import {WarningComponent} from './LoginScreen';
+import AsyncStorage from '@react-native-community/async-storage';
+import axios from 'axios';
+const API_SERVER_URL = 'https://hidden-refuge-96933.herokuapp.com/';
 
-export default function ChangePasswordScreen() {
+export default function ChangePasswordScreen({navigation}) {
 
+  const {signOut} = React.useContext(AuthContext);
   const [oldPassword, setOldPassword] = useState('');
   const [newPassword, setNewPassword] = React.useState('');
 
   const [invalidOldPassword, setInvalidOldPassword] = useState(false);
   const [invalidPassword, setInvalidPassword] = useState(false);
   const [wrongVerifyPassword, setWrongVerifyPassword] = useState(false);
-  
-  const { error, isLoading } = React.useContext(RegisterContext);
 
-  
+  const {error, isLoading} = React.useContext(RegisterContext);
+
   const onChangeOldPassword = (text) => {
     if (text !== '') {
       setInvalidOldPassword(false);
-      setOldPassword(text)
+      setOldPassword(text);
     } else {
       setInvalidOldPassword(true);
     }
-  }
+  };
 
   const onChangePassword = (text) => {
-    if (text == '' || text===oldPassword) setInvalidPassword(true);
+    if (text == '' || text === oldPassword) setInvalidPassword(true);
     else {
       setInvalidPassword(false);
-      setNewPassword(text)
+      setNewPassword(text);
     }
-
-  }
+  };
 
   const onChangeVerifyPassword = (text) => {
     setWrongVerifyPassword(text != newPassword);
-  }
+  };
 
-  const changePassword = () => {
+  const changePassword = async () => {
     // TODO: gọi API change password, nhận và xử lí response
-    
+    let token = '';
+    try {
+      token = await AsyncStorage.getItem('userToken');
+    } catch (e) {
+      console.log('Restoring token failed');
+      console.log(e);
+    }
     // Example:
-    // axios.post(API_SERVER_URL + 'change_password', {
-    //   oldPassword: oldPassword,
-    //   newPassword: newPassword
-    // })
-    // .then(function (response) {
+    console.log({
+      token,
+      password: oldPassword,
+      newPassword: newPassword,
+    });
+    axios
+      .post(API_SERVER_URL + 'change_password', {
+        token,
+        password: oldPassword,
+        newPassword: newPassword,
+      })
+      .then(function (response) {
+        console.log(response.data);
+        if (response.data.code == 1000) {
+          alert(response.data.message);
+          navigation.navigate('NewfeedScreen');
+        }
+        if (response.data.code == 1) {
+          alert(response.data.message);
+          signOut()
+        }
+        if (response.data.code == 0) {
+          alert(response.data.message);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-    alert('changed password');
-  }
-
-  return(
+  return (
     <View style={styles.container}>
-      <Spinner visible={isLoading}/>
+      <Spinner visible={isLoading} />
       <View style={styles.topWrapper}>
-        <TextInput style={styles.textInput} secureTextEntry={true} placeholder='Mật khẩu cũ' onChangeText={onChangeOldPassword} />
-        {invalidOldPassword ? <WarningComponent text='Điền mật khẩu cũ không được để trống'/> : null}
-        <TextInput style={styles.textInput} secureTextEntry={true} placeholder='Mật khẩu mới' onChangeText={onChangePassword}/>
-        {invalidPassword ? (newPassword==='' ? <WarningComponent text='Mật khẩu mới không được để trống'/> :<WarningComponent text='Điền mật khẩu cũ phải khác mật khẩu mới'/>): null}
-        <TextInput style={styles.textInput} secureTextEntry={true} placeholder='Nhập lại mật khẩu mới' onChangeText={onChangeVerifyPassword}/>
-        {wrongVerifyPassword ? <WarningComponent text='Mật khẩu mới không khớp'/> : null}
+        <TextInput
+          style={styles.textInput}
+          secureTextEntry={true}
+          placeholder="Mật khẩu cũ"
+          onChangeText={onChangeOldPassword}
+        />
+        {invalidOldPassword ? (
+          <WarningComponent text="Điền mật khẩu cũ không được để trống" />
+        ) : null}
+        <TextInput
+          style={styles.textInput}
+          secureTextEntry={true}
+          placeholder="Mật khẩu mới"
+          onChangeText={onChangePassword}
+        />
+        {invalidPassword ? (
+          newPassword === '' ? (
+            <WarningComponent text="Mật khẩu mới không được để trống" />
+          ) : (
+            <WarningComponent text="Điền mật khẩu cũ phải khác mật khẩu mới" />
+          )
+        ) : null}
+        <TextInput
+          style={styles.textInput}
+          secureTextEntry={true}
+          placeholder="Nhập lại mật khẩu mới"
+          onChangeText={onChangeVerifyPassword}
+        />
+        {wrongVerifyPassword ? (
+          <WarningComponent text="Mật khẩu mới không khớp" />
+        ) : null}
         <View style={{marginTop: 20}}>
-            {
-              error === 'WRONG_PASSWORD' ? <WarningComponent text='Mật khẩu cũ không đúng'/> :
-              error === 'NET_ERR' ? <WarningComponent text='Lỗi kết nối'/> : 
-              error === 'UNKNOWN' ? <WarningComponent text='Lỗi không xác định' /> : null
-            }
+          {error === 'WRONG_PASSWORD' ? (
+            <WarningComponent text="Mật khẩu cũ không đúng" />
+          ) : error === 'NET_ERR' ? (
+            <WarningComponent text="Lỗi kết nối" />
+          ) : error === 'UNKNOWN' ? (
+            <WarningComponent text="Lỗi không xác định" />
+          ) : null}
         </View>
-        <Button color={'#2979FF'} title='Đổi mật khẩu' onPress={() => {
-          if (!wrongVerifyPassword && !invalidOldPassword && oldPassword != '') changePassword({ oldPassword: oldPassword, newPassword: newPassword })
-        }}/>
+        <Button
+          color={'#2979FF'}
+          title="Đổi mật khẩu"
+          onPress={() => {
+            if (
+              !wrongVerifyPassword &&
+              !invalidOldPassword &&
+              oldPassword != ''
+            )
+              changePassword({
+                oldPassword: oldPassword,
+                newPassword: newPassword,
+              });
+          }}
+        />
       </View>
     </View>
   );
@@ -86,11 +159,11 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'column',
     // justifyContent: 'space-between',
-    flex: 1
+    flex: 1,
   },
   registerImg: {
     marginLeft: 'auto',
-    marginRight: 'auto'
+    marginRight: 'auto',
   },
   topWrapper: {
     flex: 1,
@@ -111,6 +184,6 @@ const styles = StyleSheet.create({
   warningText: {
     textAlign: 'center',
     color: '#FF5252',
-    marginRight: 10
-  }
+    marginRight: 10,
+  },
 });
